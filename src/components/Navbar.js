5086f30b4937f5f87897c2338e6b97b8323c0e71
ngobi808 <kevongobiro@gmail.com>
2019-07-18 11:50:11 +0300

    pigalooku

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
-import logo from '../home.png';
+import logo from '../looku.png';
 import styled from 'styled-components';
 import {ButtonContainer} from "./Button";
 
@@ -14,7 +14,7 @@ export default class Navbar extends Component {
             <ul className="navbar-nav align-items-center">
             <li className="nav-item ml-5">
             <Link to="/" className="nav-link">
-                products
+                STORE
             </Link>
             </li>
            </ul>
@@ -34,7 +34,7 @@ export default class Navbar extends Component {
 }
 
 const NavWrapper = styled.nav`
-background: var(--mainGreen);
+background: var(--mainBlue);
 .nav-link {
     color:var(--mainWhite)!important;
     font-size:1.3rem;
